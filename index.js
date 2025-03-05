c46a8068f7c2202a08d56a5bab9206c83ea1fc11
bilow osmoo <bilowosmo@gmail.com>
2025-03-05 17:10:53 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+// The initial array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive methods
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-destructive methods
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+// Export functions if needed for testing
+module.exports = {
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat
+};
